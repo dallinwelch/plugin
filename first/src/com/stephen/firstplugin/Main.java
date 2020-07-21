a194e57575c2336de71110eca499475a8988fad6
dallin welch <67934524+dallinwelch@users.noreply.github.com>
2020-07-21 11:00:35 -0600

    Add files via upload

---
+++ first/src/com/stephen/firstplugin/Main.java
@@ -0,0 +1,16 @@
+package com.stephen.firstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		System.out.println("first plugin has worked!");
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
